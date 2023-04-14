2f58ddea35bdd47a8830672e59e578981d72c6ba
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-04-14 11:12:16 -0700

    Update calculate-product-of-array-elements.js

--- calculate-product-of-array-elements.js
+++ calculate-product-of-array-elements.js
@@ -1,5 +1,5 @@
 //Use .reduce() to calculate the product of the elements in the input array.
 
 let elementProduct = function(arr) {
-  return arr.reduce((acc, curr) => acc * curr)
+  return arr.reduce((acc, curr) => acc * curr, 1)
 }
