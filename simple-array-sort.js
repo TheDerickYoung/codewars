692e8a2330a766429970771e2e37ffcf2cd4b679
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-04-14 11:14:16 -0700

    Update simple-array-sort.js

--- simple-array-sort.js
+++ simple-array-sort.js
@@ -1,5 +1,5 @@
 //Use .sort() to sort the elements of the input array in descending order.
 
 let sortArr = function(arr) {
-  return arr.sort((a, b) => a > b ? 1 : -1)
+  return arr.sort((a, b) => b - a)
 }
