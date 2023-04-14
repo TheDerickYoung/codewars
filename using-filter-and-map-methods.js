d913bec6f555bc49f488a9a4e90c1c1dff009206
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-04-14 11:15:47 -0700

    Update using-filter-and-map-methods.js

--- using-filter-and-map-methods.js
+++ using-filter-and-map-methods.js
@@ -1,6 +1,5 @@
 //Use .filter() and .map() to create a new array that contains only the even numbers from the input array, and then square each even number in the resulting array.
 
 let evenSquares = function(arr) {
-  let evens = arr.filter(element => element % 2 === 0)
-  return evens.map(element => Math.pow(element, 2))
+  return arr.filter(element => element % 2 === 0).map(element => Math.pow(element, 2))
 }
