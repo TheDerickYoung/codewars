02f50d6b737ebd7a1ea636d4d4b5edb3b1e5bbce
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-04-16 07:43:20 -0700

    Create new-person-object.js

---
+++ new-person-object.js
@@ -0,0 +1,12 @@
+//Create an object called "person" with the following properties:
+//firstName (string)
+//lastName (string)
+//age (number)
+//email (string)
+
+let person = {
+  firstName: 'John',
+  lastName: 'Wilkerson',
+  age: 46,
+  email: 'johnwilkersonisfake23@yahoo.com'
+}
