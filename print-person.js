5831e559cc25d0d6e5d0c7e0da2e8acb835e29ad
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-04-16 07:56:11 -0700

    Update print-person.js

--- print-person.js
+++ print-person.js
@@ -1,6 +1,6 @@
 //Create a function called "printPerson" that takes a person object as an argument and prints out their full name and email 
 //in the following format: "Full Name: [firstName] [lastName], Email: [email]"
 
-let printPerson = function(obj) {
+function printPerson(obj) {
   console.log(`First Name: ${obj.firstName}, Last Name: ${obj.lastName}, Email: ${obj.email}`)
 }
