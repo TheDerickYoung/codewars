62cc10b017ca9e382d8bf94d86088e33e1186f16
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-04-26 14:21:11 -0700

    Create alternating-split.js

---
+++ alternating-split.js
@@ -0,0 +1,43 @@
+//Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed characters of S with all the even-indexed characters of S, this process should be repeated N times.
+
+//Examples:
+
+//encrypt("012345", 1)  =>  "135024"
+//encrypt("012345", 2)  =>  "135024"  ->  "304152"
+//encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+
+//encrypt("01234", 1)  =>  "13024"
+//encrypt("01234", 2)  =>  "13024"  ->  "32104"
+//encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
+//Together with the encryption function, you should also implement a decryption function which reverses the process.
+
+//If the string S is an empty value or the integer N is not positive, return the first argument without changes.
+
+function encrypt(text, n) {
+    if (!text || n <= 0) {
+      return text;
+    }
+  for (let i = 1; i <= n; i++) {
+    const odds = text.split('').filter((element, index) => index % 2 !== 0).join('')
+    const evens = text.split('').filter((element, index) => index % 2 === 0).join('')
+    text = odds + evens
+  }
+  return text
+}
+
+
+function decrypt(encryptedText, n) {
+  if (!encryptedText || n <= 0) {
+    return encryptedText;
+  }
+  let result = encryptedText;
+  const mid = Math.floor(result.length / 2);
+  for (let i = 1; i <= n; i++) {
+    let temp = "";
+    for (let j = 0; j < mid; j++) {
+      temp += result[mid + j] + result[j];
+    }
+    result = temp + result.substr(mid * 2);
+  }
+  return result;
+}
