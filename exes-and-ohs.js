ee521849021b85a7590f1c292517b4c20ead7081
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-04-27 13:59:38 -0700

    Create exes-and-ohs.js

---
+++ exes-and-ohs.js
@@ -0,0 +1,23 @@
+//Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+//Examples input/output:
+
+//XO("ooxx") => true
+//XO("xooxx") => false
+//XO("ooxXm") => true
+//XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+//XO("zzoo") => false
+
+function XO(str) {
+  let xCount = 0;
+  let oCount = 0;
+  let lowercaseStr = str.toLowerCase();
+  for (let i = 0; i < lowercaseStr.length; i++) {
+    if (lowercaseStr[i] === 'x') {
+      xCount++;
+    } else if (lowercaseStr[i] === 'o') {
+      oCount++;
+    }
+  }
+  return xCount === oCount;
+}
