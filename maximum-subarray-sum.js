ab558e94e5e74be9f9f0f7d2ae561625be96d9a2
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-05-10 10:58:31 -0700

    Update maximum-subarray-sum.js

--- maximum-subarray-sum.js
+++ maximum-subarray-sum.js
@@ -1,3 +1,4 @@
+//5kyu
 //The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
 
 //maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
