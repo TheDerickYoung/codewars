f7329e43e4509c62f23350912f1c7faa20468b39
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-05-15 13:09:52 -0700

    Create traffic-light.js

---
+++ traffic-light.js
@@ -0,0 +1,21 @@
+//You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+//Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+//For example, when the input is green, output should be yellow.
+
+function updateLight(current) {
+  
+const colors = { 
+  green: 'yellow',
+  yellow: 'red',
+  red: 'green',
+}
+  if (current === colors.red) {
+    return colors.green
+  } else if (current === colors.green) {
+    return colors.yellow
+  } else if (current === colors.yellow) {
+    return colors.red
+  }
+}
