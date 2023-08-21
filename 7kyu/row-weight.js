d6888937a47f5071c722e439bc6d8b5b30a41715
Derick Young Jr <119906716+TheDerickYoung@users.noreply.github.com>
2023-08-20 22:43:20 -0700

    Update row-weight.js

--- 7kyu/row-weight.js
+++ 7kyu/row-weight.js
@@ -6,8 +6,8 @@
 //Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
 
 function rowWeights(array) {
-  const evenTeam = array.filter((element, index) => index % 2 === 0);
-  const oddTeam = array.filter((element, index) => index % 2 !== 0);
+  const evenTeam = array.filter((element, index) => index % 2 === 0).reduce((a,b) => a + b, 0);
+  const oddTeam = array.filter((element, index) => index % 2 !== 0).reduce((a,b) => a + b, 0);
 
-  return [evenTeam.reduce((a,b) => a + b, 0), oddTeam.reduce((a,b) => a + b, 0)];
+  return [evenTeam, oddTeam];
 }
