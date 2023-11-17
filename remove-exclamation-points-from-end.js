8e6ff2787e01ce586e62414f2babacd639c1fe18
Derick Young Jr <119906716+TheDerickYoung@users.noreply.github.com>
2023-11-16 21:38:18 -0800

    Create remove-exclamation-points-from-end.js

---
+++ remove-exclamation-points-from-end.js
@@ -0,0 +1,13 @@
+//Remove all exclamation marks from the end of sentence.
+
+//Examples
+//"Hi!"     ---> "Hi"
+//"Hi!!!"   ---> "Hi"
+//"!Hi"     ---> "!Hi"
+//"!Hi!"    ---> "!Hi"
+//"Hi! Hi!" ---> "Hi! Hi"
+//"Hi"      ---> "Hi"
+
+function remove (string) {  
+  return string.replace(/!+$/, '');
+}
